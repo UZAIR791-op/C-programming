504a92f09911ecd0eaf21f022da2906eaf7a5f8b
UZAIR791-op <uzairkhan123@eng.rizvi.edu.in>
2024-12-11 22:39:14 +0530

    Create Task1.c

---
+++ Expt1/Task1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+printf("Full Name\t: Uzair Khan \n");
+
+printf("Age\t\t: 26\n");
+
+printf("Class\t\t: FE\n");
+
+printf("Division\t: D\n");
+
+printf("UIN\t\t: 241P005\n");
+
+return 0;
+
+}
